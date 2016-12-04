4ab526e07b539d7861c2ab6eb35e4c4e2d68d71e
Gabriel Theodoropoulos <gabriel@gabriels-mbp.lan>
2016-12-04 14:11:42 +0200

    Initial Commit

---
+++ Constraints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Gabriel Theodoropoulos on 04/12/16.
+//  Copyright © 2016 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
