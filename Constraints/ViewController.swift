75dc41258342923da92edfc2c90ed0e3f530f9d7
hawk <hawk@clacomusic.com>
2016-12-27 16:05:24 +0800

    origin_contraints_file

--- Constraints/ViewController.swift
+++ Constraints/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var containerView1: UIView!
+    
+    var containerView2: UIView!
+    
+    var containerView3: UIView!
+    
+    var label1: UILabel!
+    
+    var label2: UILabel!
+    
+    var button1: UIButton!
+    
+    var button2: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        simpleView1()
+        
+//        simpleView2()
+        
+//        complexView1()
+        
+//        complexView2()
+        
+//        vflExample()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    func simpleView1() {
+        
+    }
+    
+    
+    func simpleView2() {
+        
+    }
+    
+    
+    func complexView1() {
+        
+    }
+    
+    
+    func complexView2() {
+        
+    }
+    
+    
+    func vflExample() {
+        
+    }
 }
 
